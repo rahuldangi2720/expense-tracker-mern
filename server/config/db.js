3266f6c38bb2e959598e339b50e2ad6dbe29ec2e
rahuldangi2720 <rahuldangi905019@gmail.com>
2025-04-15 16:41:54 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,15 @@
-const {default : mongoose}= require("mongoose");
-exports.connectDB= async()=>{
-    try {
-        // const connect = await mongoose.connect(process.env.MONGO_URL);
-        const connect = await mongoose.connect("mongodb+srv://rohit1447481:3TuByNRJGNF0Ox5O@cluster0.ij0ki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const { default: mongoose } = require("mongoose");
+exports.connectDB = async () => {
+  try {
+    // const connect = await mongoose.connect(process.env.MONGO_URL);
+    const connect = await mongoose.connect(
+      "mongodb+srv://rahuldangi905019:rahul321@cluster0.svieqqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
 
-        if(connect){
-            console.log("MongoDb is connected");
-            
-        }
-    } catch (error) {
-        console.log(error);
-        
+    if (connect) {
+      console.log("MongoDb is connected");
     }
-}
-
+  } catch (error) {
+    console.log(error);
+  }
+};
